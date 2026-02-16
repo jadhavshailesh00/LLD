2994ba2bbc7e8783b14aea0b6a78fa03aeee0de6
shailesh jadhav <jadhavshailesh00@gmail.com>
2026-02-16 20:08:03 +0530

    Added Observer,Template

---
+++ behavioral design patterns/Observer/EmailNotifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLD.behavioral_design_patterns.Observer
+{
+    public class EmailNotifier : IObserver
+    {
+        public void Update(decimal price)
+        {
+            Console.WriteLine($"Email: Stock price updated to {price}");
+        }
+    }
+
+}
---
+++ behavioral design patterns/Template/ExcelReportGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLD.behavioral_design_patterns.Template
+{
+    public class ExcelReportGenerator : ReportGenerator
+    {
+        protected override void FormatData()
+        {
+            Console.WriteLine("Formatting data as Excel...");
+        }
+    }
+}
